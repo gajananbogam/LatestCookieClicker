502072c040bc2da1771197cf151132b2829ddc35
Gajanan Bogam <Gajanan.Bogam@cqc.org.uk>
2023-05-14 19:49:39 +0100

    first commit

---
+++ CookieClicker_RegressionTests/LandingPageTests.cs
@@ -0,0 +1,52 @@
+﻿
+using CookieClicker_Automation;
+using FluentAssertions;
+using Xunit;
+
+namespace CookieClicker_RegressionTests
+   
+{
+    [Collection("Sequential")]
+    public class LandingPageTests : TestBase
+    {
+        public LandingPageTests()
+        {
+            LaunchCoockieClickerApplication();
+        }
+
+        [Fact]
+        public void VerifyTextOfCookieClickerLandingPage()
+        {
+            landingPage.PageTitle.Text.Should().Be(Constants.CookieClickerTitle);
+            landingPage.NewGameSectionHeading.Text.Should().Be(Constants.NewGameSectionTitle);
+            landingPage.HighScoresSectionHeading.Text.Should().Be(Constants.HighScoresSectioTitle);
+        }
+
+        [Fact]
+        public void VerifyNewGameSectionAtLandingPage()
+        {
+            landingPage.StartButton.Displayed.Should().BeTrue();
+            landingPage.YourNameTextInputBox.Displayed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void VerifyHighScoresSection()
+        {
+            landingPage.HighScoresSectionHeading.Text.Should().Be(Constants.HighScoresSectioTitle);
+            landingPage.PlayerTitle.Text.Should().Be(Constants.PlayerSubSectionTitle);
+            landingPage.ScoreTitle.Text.Should().Be(Constants.ScoresSubSectionTitle);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" Airelogic QA Team")]
+        [InlineData(" @@@@@@@")]
+        [InlineData(" I am entering the string which is very long, ideally this long string should not be allowed at the name field")]
+        public void VerifyYourNameTextboxFunctionality(string name)
+        {
+            landingPage.YourNameTextInputBox.SendKeys(name);
+            landingPage.StartButton.Click();
+            startPage.WelcomeMessage.Text.Should().Be(Constants.WelcomeMessage + name);
+        }
+    }
+}
---
+++ CookieClicker_RegressionTests/StartPageTests.cs
@@ -0,0 +1,70 @@
+﻿
+using CookieClicker_Automation;
+using FluentAssertions;
+using Xunit;
+
+namespace CookieClicker_RegressionTests
+{
+    [Collection("Sequential")]
+    public class StartPageTests : TestBase
+    {
+        public StartPageTests()
+        {
+            LaunchCoockieClickerApplication();
+            StartTheGameByEnteringPlayerName();
+        }
+
+        [Fact]
+        public void VerifyTheSectionsOfStartPage()
+        {
+            // Verifyiong the Hello message at the Landing page.
+            startPage.WelcomeMessage.Displayed.Should().BeTrue();
+            // Verifying Cookies section is present with the default 0 value
+            startPage.Cookies.Displayed.Should().BeTrue();
+            startPage.Cookies.Text.Should().Be(Constants.Zero);
+            // Verifying Factories section is present woith default 0 value
+            startPage.Factories.Displayed.Should().BeTrue();
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+            // Verifying Moneys section is present woith default 0 value
+            startPage.Money.Displayed.Should().BeTrue();
+            WaitForApplictionToLaodOrSync();
+            startPage.Money.Text.Should().Be(Constants.Zero);           
+            startPage.ClickCookieButton.Displayed.Should().BeTrue();
+            startPage.SellCookiesInputBox.Displayed.Should().BeTrue();
+            startPage.SellCookiesButton.Displayed.Should().BeTrue();
+            startPage.BuyFactoriesInputBox.Displayed.Should().BeTrue();
+            startPage.BuyFactoriesButton.Displayed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void VerifySellCookiesFunctionality()
+        {
+            CollectCookies(5);
+            WaitForApplictionToLaodOrSync();
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.SpecialCharacter);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.DecimalValue);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.Zero);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.NegativeValue);
+            startPage.Cookies.Text.Should().Be("5");
+        }
+
+        [Theory]
+        [InlineData(Constants.SpecialCharacter)]
+        [InlineData(Constants.DecimalValue)]
+        [InlineData(Constants.Zero)]
+        [InlineData(Constants.ValidNumber)]
+        public void VerifyBuyFactories(string value)
+        {
+            WaitForApplictionToLaodOrSync();
+            startPage.Cookies.Text.Should().Be(Constants.Zero);
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+            startPage.Money.Text.Should().Be(Constants.Zero);
+            BuyFactories(value);
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+        } 
+    }
+}
